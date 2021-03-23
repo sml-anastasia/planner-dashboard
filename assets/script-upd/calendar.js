9908c6f85e87fdf0a7a2dd95c7d7dc803bb52749
anastasia <smolina.anastasia.14@gmail.com>
2021-03-23 14:39:15 +0300

    refactor: add fixed files

---
+++ assets/script-upd/calendar.js
@@ -0,0 +1,68 @@
+let calendar = new tui.Calendar('#calendar', {
+        taskView: false,
+        scheduleView: false,
+        week: {
+            startDayOfWeek: 1,
+        }
+    });
+    function moveToNextWeek() {
+        calendar.next();
+    };
+    function moveToPrevWeek() {
+        calendar.prev();
+};
+
+var myPlanner = [];
+//{
+// date: '',
+// todo: [],
+// notes: [],
+// deadlines: [],
+// habits: []
+// }];
+
+if (!Date.prototype.toISODate) {
+    Date.prototype.toISODate = function() {
+        return this.getFullYear() + '-' + ('0' + (this.getMonth() + 1)).slice(-2) + '-' + ('0' + this.getDate()).slice(-2);
+    }
+}
+let today = new Date().toISODate();
+
+function updateURL(date) {
+    var baseUrl = window.location.protocol + "//" + window.location.host + window.location.pathname;
+    var newUrl = baseUrl + '?date=' + date;
+    window.location.href = newUrl;
+}
+
+calendar.on('clickDayname', function(event) {
+    myPlanner.date = event.date;
+    updateURL(event.date);
+    generateDashboard(event.date);
+    console.log(myPlanner);
+});
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    let today = getParameterByName('date');
+    generateDashboard(today);
+});
+
+function getParameterByName(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+function generateDashboard(date) {
+    console.log(date);
+    let todayTasks = myPlanner.find(x=>x.date==date);
+    let todayNotes = myPlanner.find(x=>x.date==date);
+    console.log(todayTasks);
+    console.log(todayNotes);
+    // localStorage.setItem('planner', JSON.stringify(myPlanner)); // если эту строчку использовать, то планнер при переключении на другую дату обнуляется в хранилище
+
+    //document.getElementById('taskContainer').innerHTML = dashboardDiv;
+    //document.getElementById('dashboard').setAttribute('date', date);
+}
---
+++ assets/script-upd/htracker.js
@@ -0,0 +1,90 @@
+// let habitsNamesList = [];
+// let habitsInputs = [];
+let habitBox= document.querySelector("#habit_box");
+let allHabits = [];
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    allHabits = JSON.parse(localStorage.getItem('habits'));
+        if (allHabits != null) {
+        appendHabit();
+    }
+
+    appendHabit();
+}); 
+
+function addHabit() {
+    let newHabit = {checks:[false,false,false,false,false,false,false]};
+    let habitName = document.getElementById("habit_input");
+
+    if (habitName.value == ""){
+        return alert("Please enter your habit");
+    } else {
+        newHabit.name = habitName.value;
+    }
+    appendHabit();
+    allHabits.push(newHabit);
+}
+
+function appendHabit(){
+    let habits = document.querySelectorAll(".habit-item");
+    if (habits.length > 0) {
+        habits.forEach(habit => {
+        habit.remove();
+})
+}
+
+    let date = getParameterByName('date');
+    
+    //console.log(myPlanner.find(x=>x.date==date).habits);
+
+    if(myPlanner.find(x=>x.date==date)) {
+    myPlanner.find(x=>x.date==date).habits.map(habit => {
+    let habitItem = document.createElement('div');
+    habitItem.classList = 'habit-item';
+    let habitTitle = document.createElement('span');
+    habitTitle.innerText = habit.name;
+    habitTitle.classList = 'habit-title';
+    let habitForm = document.createElement('form');
+    // habitForm.innerText = note.note;
+    habitForm.classList = 'habit-form';
+    // habitForm.setAttribute("onchange", "isChecked()");
+    // let habitDelete = document.createElement('img');
+    // habitDelete.src = 'assets/images/note-icon-delete.svg';
+    // habitDelete.classList.add('habit-delete');
+    let habitCheck;
+    for (let i=0; i<7; i++){
+        habitCheck = document.createElement('input');
+        habitCheck.setAttribute("type", "checkbox");
+        habitCheck.setAttribute("name",`checkbox-${habit.name}`);
+        if (habit.checks[i]) habitCheck.setAttribute("checked", "checked");
+        //habitCheck.onclick = 
+        // habitCheck.type = 'checkbox';
+        // habitCheck.name = `checkbox-${habit.name}`; 
+        habitForm.appendChild(habitCheck);
+    }
+
+
+    habitItem.appendChild(habitTitle);
+    habitItem.appendChild(habitForm);
+    
+    habitBox.appendChild(habitItem);
+    
+    // getDeleteButtons();
+    localStorage.setItem('habits', JSON.stringify(allHabits));
+    }) ;
+    }
+}
+// let checkboxes = [];
+
+// function isChecked() {
+//     let inputs = document.getElementsByName("checkbox-"+ habitName.value);
+//     for (let input of inputs) {
+//         if (input.checked) {
+//             checkboxes.push("1");
+//         } else {
+//             checkboxes.push("0");
+//         }
+//     }
+//     console.log(habitName.value);
+//     console.log(checkboxes);
+// }
